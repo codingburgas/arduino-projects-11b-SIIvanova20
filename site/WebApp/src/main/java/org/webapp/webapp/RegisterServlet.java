a0463aeacb9342c2320153ce6e089894851dbde8
Stella Ivanova <81414056+SIIvanova20@users.noreply.github.com>
2024-06-29 22:16:31 +0300

    Update register servlet

--- site/WebApp/src/main/java/org/webapp/webapp/RegisterServlet.java
+++ site/WebApp/src/main/java/org/webapp/webapp/RegisterServlet.java
@@ -1,5 +1,8 @@
 package org.webapp.webapp;
 
+import BusinessLayer.UserService;
+import DataAccessLayer.repositories.UserRepository;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,8 +12,21 @@ import java.io.IOException;
 
 @WebServlet("/register")
 public class RegisterServlet extends HttpServlet {
+
+    private static UserService userServiceInstance = UserService.getUserServiceInstance();
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("views/shared/register.jsp").forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        String deviceCode = request.getParameter("deviceCode");
+
+        userServiceInstance.CreateUser(username, email, password, deviceCode);
+
+        response.sendRedirect(request.getContextPath() + "/index.jsp");
     }
 
 }
